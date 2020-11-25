ffa46a97baf97895198794094c2ee1defc8cb962
leipan <leipan@jpl.nasa.gov>
2020-11-24 17:50:10 -0800

    init

---
+++ c/bridge.py
@@ -0,0 +1,37 @@
+import shlex, subprocess
+import os
+import time
+
+command_line = input('What program to run: ')
+
+DMTCP_ROOT = os.environ.get('DMTCP_ROOT')
+DEMO_PORT = os.environ.get('DEMO_PORT')
+
+### dmtcp = DMTCP_ROOT+'/bin/dmtcp_launch --quiet --coord-port '+DEMO_PORT+' --with-plugin libdmtcp_plugin-to-announce-events.so '
+dmtcp = DMTCP_ROOT+'/bin/dmtcp_launch --quiet --coord-port '+DEMO_PORT+' '
+command_line = dmtcp + command_line
+
+# run with dmtcp_launch
+args = shlex.split(command_line)
+print(args)
+p = subprocess.Popen(args)
+p.wait()
+
+### time.sleep(10)
+
+# run dmtcp_restart_script.sh
+command_line = 'dmtcp_restart_script.sh --coord-host higgs.jpl.nasa.gov'
+args = shlex.split(command_line)
+print(args)
+p = subprocess.Popen(args)
+p.wait()
+
+### time.sleep(10)
+
+# run dmtcp_restart_script.sh
+command_line = 'dmtcp_restart_script.sh --coord-host higgs.jpl.nasa.gov'
+args = shlex.split(command_line)
+print(args)
+p = subprocess.Popen(args)
+
+print('')
