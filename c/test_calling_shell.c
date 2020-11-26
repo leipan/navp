d3d8938bd0d2e32c249d4e50ae87b718c76fa906
leipan <leipan@jpl.nasa.gov>
2020-11-26 14:44:42 -0800

    init

---
+++ c/test_calling_shell.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+/* ls -al | grep '^d' */
+  FILE *pp;
+  pp = popen("curl http://higgs.jpl.nasa.gov:8080 1>&2", "r");
+  if (pp != NULL) {
+    while (1) {
+      char *line;
+      char buf[1000];
+      line = fgets(buf, sizeof buf, pp);
+      if (line == NULL) break;
+      if (line[0] == 'd') printf("%s", line); /* line includes '\n' */
+    }
+    pclose(pp);
+  }
+  return 0;
+}
