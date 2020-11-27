da28c6745a1545bc1a060f39eb1fcefd134b2708
leipan <leipan@jpl.nasa.gov>
2020-11-27 10:39:30 -0800

    made a func call_shell_command()

--- c/test_calling_shell.c
+++ c/test_calling_shell.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int call_shell_command(char *shell_command) {
 /* ls -al | grep '^d' */
   FILE *pp;
-  pp = popen("curl http://higgs.jpl.nasa.gov:8080/svc/navp_hop 1>&2", "r");
+  /* pp = popen("curl http://higgs.jpl.nasa.gov:8080/svc/navp_hop 1>&2", "r"); */
+  pp = popen(shell_command, "r");
   if (pp != NULL) {
     while (1) {
       char *line;
@@ -15,5 +16,16 @@ int main(void) {
     }
     pclose(pp);
   }
+  return 0;
+}
+
+
+int main(void) {
+
+  /* char *cmd = "curl http://higgs.jpl.nasa.gov:8080/svc/navp_hop 1>&2"; */
+  char *cmd = "curl http://higgs.jpl.nasa.gov:8080 1>&2";
+
+  call_shell_command(cmd);
+
   return 0;
 }
