bdec42f2019369646f3967187aaa363559c601e0
Lei Pan <leipan@jpl.nasa.gov>
2020-12-10 08:51:31 -0800

    used resume to avoid an infinite loop

--- python/test_numpy.py
+++ python/test_numpy.py
@@ -4,8 +4,15 @@ a=numpy.array([0,1,2])
 print('before checkpoint: ', a)
 dmtcp.checkpoint()
 print('after checkpoint: ', a)
-a=numpy.array([0,100,200])
-print('before restore: ', a)
-dmtcp.restore()
-print('after restore: ', a)
+
+# resuming after checkpoint, not restarting from checkpoint
+if dmtcp.isResume():
+  a=numpy.array([0,100,200])
+  print('before restore: ', a)
+  dmtcp.restore()  # this restarts from checkpoint
+
+  # this will never be printed out
+  # cause in case of resuming from checkpoint, the line above (restore) will cause restarting from checkpoint
+  # otherwise in case of restarting from checkpoint, this entire if block will not be run at all
+  print('after restore: ', a)  
 
