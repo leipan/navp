1f340c631ccd34636e472bf3c1b3a05955669d87
leipan <leipan@jpl.nasa.gov>
2021-06-17 12:33:17 -0700

    made simpler by removing the redirection 1>&2

--- run_matchup.sh
+++ run_matchup.sh
@@ -5,17 +5,15 @@ BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 # source environment
 source $HOME/verdi/bin/activate
 
-echo "##########################################" 1>&2
-echo -n "Running WVCC matchup: " 1>&2
-date 1>&2
+echo "##########################################"
+echo -n "Running WVCC matchup: "
+date
 cd $HOME/data
 python $BASE_PATH/../CrIS_VIIRS_collocation-master/code_test_QY.py
 STATUS=$?
-echo -n "Finished running WVCC matchup: " 1>&2
-date 1>&2
+echo -n "Finished running WVCC matchup: "
+date
 if [ $STATUS -ne 0 ]; then
-  echo "Failed to run WVCC matchup." 1>&2
-  cat code_test.log 1>&2
-  echo "{}"
+  echo "Failed to run WVCC matchup."
   exit $STATUS
 fi
