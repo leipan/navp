e0393e15145f9345a95db7fb444fecf372834643
leipan <leipan@jpl.nasa.gov>
2021-08-09 11:13:32 -0700

    added print statement

--- services/svc/run.py
+++ services/svc/run.py
@@ -1,6 +1,7 @@
 from svc import app
 
 if __name__ == '__main__':
+  print('port: ', app.config['PORT'])
   app.run(host="0.0.0.0", port=app.config['PORT'], debug=True)
   ### app.run(host="0.0.0.0", port=8080, debug=True)
   ### app.run(ssl_context=('cert.pem', 'key.pem'), host="0.0.0.0", port=app.config['PORT'], debug=True)
