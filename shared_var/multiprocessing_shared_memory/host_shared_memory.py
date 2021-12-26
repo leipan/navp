8d620534c3c7a6312b329a318614f9c2463ed893
leipan <leipan@jpl.nasa.gov>
2021-12-25 19:01:50 -0800

    clean up

--- shared_var/multiprocessing_shared_memory/host_shared_memory.py
+++ shared_var/multiprocessing_shared_memory/host_shared_memory.py
@@ -1,20 +1,9 @@
 from multiprocessing.managers import SharedMemoryManager
 from multiprocessing import shared_memory, resource_tracker
 
-"""
-from .resource_tracker import register
-
-create = True
-if create:
- register(self._name, "shared_memory")
-"""
-
-with SharedMemoryManager() as smm:
-  ### sl = smm.ShareableList(range(20))
+### with SharedMemoryManager() as smm:
+if True:
   shm = shared_memory.SharedMemory(name='test_smm', size=10, create=True)
-  ### resource_tracker.unregister(shm._name, 'shared_memory')
   print('shm._name: ', shm._name)
-  ### print('sl.shm.name: ', sl.shm.name)
-  ### print('sl: ', sl)
 
   input("Press Enter to continue")
