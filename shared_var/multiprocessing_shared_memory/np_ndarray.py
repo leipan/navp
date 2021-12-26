b749f407cc7fab8770b378c8ab35512826624c93
leipan <leipan@jpl.nasa.gov>
2021-12-26 01:39:55 -0800

    init

---
+++ shared_var/multiprocessing_shared_memory/np_ndarray.py
@@ -0,0 +1,26 @@
+import numpy as np
+from multiprocessing import shared_memory, resource_tracker
+
+shm_name = 'test_shm_np'
+
+data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+d_shape = (len(data),)
+d_type = np.int64
+d_size = np.dtype(d_type).itemsize * np.prod(d_shape)
+
+# In main process
+# allocate new shared memory
+shm = shared_memory.SharedMemory(name=shm_name, create=True, size=d_size)
+shm_name = shm.name
+# numpy array on shared memory buffer
+a = np.ndarray(shape=d_shape, dtype=d_type, buffer=shm.buf)
+# copy data into shared memory ndarray once
+a[:] = data[:]
+
+print('a[1]: ', a[1])
+
+input("Press Enter to continue")
+
+# In main process
+shm.close()  # close after using
+shm.unlink()  # free memory
