51dba32c76ea9e509fb088d96ececd360cdad73f
leipan <leipan@jpl.nasa.gov>
2021-12-25 19:00:53 -0800

    added exceptional handling

--- shared_var/multiprocessing_shared_memory/client_shared_memory.py
+++ shared_var/multiprocessing_shared_memory/client_shared_memory.py
@@ -1,15 +1,17 @@
 from multiprocessing import shared_memory, resource_tracker
 
 name1 = 'test_smm'
-### a = shared_memory.ShareableList(name=name1)
-shm = shared_memory.SharedMemory(name='test_smm', size=10, create=False)
-resource_tracker.unregister(shm._name, 'shared_memory')
-print('shm: ', shm)
-print('shm.buf: ', shm.buf)
-print('shm.buf[2]: ', shm.buf[2])
-shm.buf[2] += 1
-### print('a._name: ', a._name)
-### print('a: ', a)
+try:
+  shm = shared_memory.SharedMemory(name='test_smm', size=10, create=False)
+  resource_tracker.unregister(shm._name, 'shared_memory')
+  ### print('shm: ', shm)
+  print('shm.buf: ', shm.buf)
+  print('len(shm.buf): ', len(shm.buf))
+  print('shm.buf[2]: ', shm.buf[2])
+  shm.buf[2] += 1
 
-### a.shm.close()
+  for item in shm.buf:
+    print(item)
 
+except FileNotFoundError:
+  print('Error: shared memory {} does not exist'.format(name1))
