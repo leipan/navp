fa8183f08d28e3eb79951216e451542d420bf9b2
Lei Pan <lei.pan@jpl.nasa.gov>
2021-12-28 13:26:24 +0000

    first version

--- shared_var/multiprocessing_shared_memory/hop_np_ndarray.py
+++ shared_var/multiprocessing_shared_memory/hop_np_ndarray.py
@@ -1,5 +1,27 @@
+import sys,os,time
 import numpy as np
 from multiprocessing import shared_memory, resource_tracker
+sys.path.append('/home/ops/navp/services/svc/svc/src/util')
+from utils import get_host_port, get_ips, swap_ips
+sys.path.append('/home/ops/navp/python')
+import dmtcp
+
+# note:
+# this script is run from navp/shared_var/multiprocessing_shared_memory
+# and the input/output are placed in /home/ops/data/ in the docker container
+
+start_t = time.time()
+print ('current dir: ', os.getcwd())
+
+if not dmtcp.isEnabled:
+  print('Run with dmtcp, like this: dmtcp_launch python %s'%__file__)
+  sys.exit(-1)
+
+src_ip, dst_ip = get_ips()
+print('src_ip: ', src_ip)
+print('dst_ip: ', dst_ip)
+port = 7788
+
 
 d_shape = (10,)
 d_type = np.int64
@@ -18,5 +40,13 @@ try:
   print('b: ', b)
 
   ex_shm.close()  # close after using
+
+  # test if the shared memory is checkpointed and brought with hop2()
+  dmtcp.hop2(src_ip, dst_ip, port)
+
+  print("started at: ", start_t)
+  print("now at: ", float(time.time()))
+  print("*** total elapsed time: --- %.2f seconds --- " % (float(time.time() - start_t)))
+
 except FileNotFoundError:
   print('Error: shared memory {} does not exist'.format(shm_name))
